54a0abdcea2ce7f502e57330691c6c75b78317be
nomankarim8 <karimnoman97@gmail.com>
2024-08-04 09:03:11 -0700

    update grabcam.sh

--- grabcam.sh
+++ grabcam.sh
@@ -3,7 +3,7 @@
 # This script modified by Noob Hackers
 #@@2222222
 # Hearlty thanks for linuxchoice
-# If you use any part from this code, giving me the credits. Read the Lincense!
+# If you use any part from this code, giving me the credits. Read the Lincense
 clear
 termux-setup-storage
 pkg install php -y
