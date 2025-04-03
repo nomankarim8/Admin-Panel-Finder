193f130288bcefb9e05c7d21ba0c87e02b3d615c
nomankarim8 <karimnoman97@gmail.com>
2025-04-02 23:21:31 -0800

    update admin_panel_finder.py

---
+++ admin_panel_finder.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+import urllib.request
+from urllib.error import URLError, HTTPError
+
+def find_admin():
+    try:
+        with open("link.txt", "r") as f:
+            link = input("Enter Site Name (ex: example.com or www.example.com): ")
+            print("\nAvailable links:")
+            
+            for sub_link in f:
+                sub_link = sub_link.strip()
+                if not sub_link:
+                    continue
+                
+                req_link = f"http://{link}/{sub_link}"
+                req = urllib.request.Request(req_link)
+                
+                try:
+                    urllib.request.urlopen(req)
+                except HTTPError as e:
+                    continue
+                except URLError as e:
+                    continue
+                else:
+                    print("[OK] =>", req_link)
+    except FileNotFoundError:
+        print("Error: 'link.txt' not found!")
+
+def credit():
+    banner = """
+    #####################################
+    #   *** Admin Panel Finder ***      #
+    #   Script by nomankarim8 White Hat #
+    #       Stay Legal!                 #
+    #####################################
+    """
+    print(banner)
+
+credit()
+find_admin()
